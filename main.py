3dbbbb4c3a3b537c455ce5c87a9e47b9e2f76be0
You-Cheng Liang <liangyc39@gmail.com>
2018-01-03 23:52:06 +0800

    Add /remind command

--- main.py
+++ main.py
@@ -25,8 +25,23 @@ bot = tgbot.Tgbot()
 fsm = fsm.MyFSM()
 node = cryptoutil.IOTA()
 donate_addr = 'KMNGEYGMTWLCFEXSB9ZGTHNQSGUGRJRICIATMXFBDVLWHPBBQIPNXVWHARML99VVQGGMHD9VCMHIU9PXDITRHDLCWX'
+
 last_donate_time = 0
 last_donate_id = 229942559
+
+state_track = False
+track_limit = []
+pre_price = 0
+
+def initial():
+    trytes = node.getTransactions('KMNGEYGMTWLCFEXSB9ZGTHNQSGUGRJRICIATMXFBDVLWHPBBQIPNXVWHARML99VVQGGMHD9VCMHIU9PXDITRHDLCWX')
+    for tryte in trytes:
+        transaction = iota.Transaction.from_tryte_string(tryte)
+        global last_donate_time
+        if tryte.find('STALKERBOTDONATE') != -1 and transaction.timestamp > last_donate_time:
+            last_donate_time = transaction.timestamp
+    global pre_price
+    pre_price = cryptoutil.getPrice('iota')
 
 
 def update():
@@ -39,7 +54,18 @@ def update():
             bot.sendMessage(last_donate_id, 'Thank you for donating ' + str(transaction.value) + ' IOTA!')
             last_donate_time = transaction.timestamp
 
+    global pre_price
+    price = cryptoutil.getPrice('iota')
+    print('current price:', price)
+    for track_id, track_price in track_limit:
+        print('id:', track_id, 'is tracking at', track_price)
+        if pre_price < track_price and track_price < price:
+            bot.sendMessage(track_id, 'IOTA up@' + str(track_price))
+        if pre_price > track_price and track_price > price:
+            bot.sendMessage(track_id, 'IOTA down@' + str(track_price))
+
     Timer(5.0, update).start()
+
 
 @app.route('/tgWebHook', methods=['POST'])
 def webhook_handler():
@@ -50,6 +76,8 @@ def webhook_handler():
     cryptos = cryptoutil.getSupportedCryptos()
 
     message = chatreq.get('message')
+    global state_track
+    global track_limit
     if message:
         chat_id = message['chat']['id']
         msgtext = message['text']
@@ -74,6 +102,8 @@ def webhook_handler():
         elif msgtext == '/track':
             pass
         elif msgtext == '/remind':
+            bot.sendMessage(chat_id, 'Tell me which price to remind you')
+            state_track = True
             pass
         elif msgtext == '/confirm':
             pass
@@ -101,8 +131,13 @@ def webhook_handler():
         else:
             try:
                 amount = float(msgtext)
-                fsm.setAmount(amount)
-                bot.sendMessage(chat_id, 'OK, calculate price of ' + str(amount) + ' cryptos.')
+                if state_track:
+                    track_limit.append((chat_id, amount))
+                    state_track = False
+                else:
+                    fsm.setAmount(amount)
+                    bot.sendMessage(chat_id, 'OK, calculate price of ' + str(amount) + ' cryptos.')
+
             except ValueError:
                 pass
 
@@ -135,6 +170,7 @@ def webhook_handler():
     return "OK"
 
 if __name__ == '__main__' :
+    initial()
     fsm.get_graph().draw('state_diagram.png', prog = 'dot')
     Timer(5.0, update).start()
     app.run(host = server_host, port = server_port, debug = False, ssl_context = ('../ssl/public.pem', '../ssl/key.pem'))
